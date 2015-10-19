fb0318116e78cc60fb35806e2c54f0101e9d559a
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-19 10:32:52 -0600

    users can add students and see individual student by clicking first name from table

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var Students = db.get('students')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var username = req.session.user
-  res.render('index', { title: 'Express', user: username });
+  res.render('index', { title: 'Student Index', user: username });
 });
 
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -6,7 +6,46 @@ var Students = db.get('students')
 
 router.get('/', function(req, res, next) {
   var username = req.session.user
-  res.render('index', { title: 'Student List', user: username });
+  Students.find({}, function (err, students){
+    res.render('students/home', { title: 'Student List', user: username, allStudents: students });
+  })
 });
+
+router.get('/new', function (req, res, next){
+  var username = req.session.user
+  res.render('students/new', {  title: "Add a Student", user: username})
+})
+
+router.post('/new', function (req, res, next){
+  var errors = []
+  if (!req.body.firstname.trim()){
+    errors.push('Must include a first name')
+  }
+  if (!req.body.lastname.trim()){
+    errors.push('Must include a last name')
+  }
+  if (!req.body.phonenumber.match(/\d/g).length===10){
+    errors.push('Not a valid phone number')
+  }
+  if (errors.length){
+    res.render('students/new', {  title: 'Add a Student', errors: errors})
+  } else {
+    Students.insert({ firstname: req.body.firstname,
+                      lastname: req.body.lastname,
+                      phone: req.body.phonenumber
+                      })
+    res.redirect('/students')
+  }
+})
+
+router.get('/:id', function (req, res, next){
+  Students.findOne({_id: req.params.id}, function (err, student){
+    res.render('students/show', { title: "Student", 
+                                  firstname: student.firstname,
+                                  lastname: student.lastname,
+                                  phone: student.phone
+                                })
+  })
+})
 
 module.exports = router;
